ec3f61cc2236698ed2c864abce94473578bd1b15
Andy Bateman <donald.bateman@abbott.com>
2016-07-29 07:49:04 -0500

    Starting material design

--- src/app/vote/voteItem/voteItem.component.ts
+++ src/app/vote/voteItem/voteItem.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter }from '@angular/core';
 import { Option } from '../../../lib/option'
 import { BeerDescription } from '../../common/beerDescription';
 import { VoteButton } from './voteButton';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
     selector: 'vote-item',
@@ -12,11 +13,16 @@ import { VoteButton } from './voteButton';
         'a:visited, a { color: black }'
     ],
     template: `
+     <md-card>
+      <md-card-title>
         <h3><a href="{{item.url}}">{{ item.beer }}</a></h3>
+         
+      </md-card-title>
         <beer-description [beer]="item"></beer-description>
         <vote-button (vote)="voteClicked()" [text]="'Yes'"></vote-button>
+        </md-card>
     `,
-    directives: [BeerDescription, VoteButton]
+    directives: [BeerDescription, VoteButton, MD_CARD_DIRECTIVES]
 })
 export class VoteItem {
     @Input() item: any;
